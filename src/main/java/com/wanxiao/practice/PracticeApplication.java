f3a02d1cfe8c92b5fc4c6f867ea83128e68b3615
wanxiao <wan_xiao@126.com>
2021-01-12 14:40:44 +0800

    整合ApiBoot

--- src/main/java/com/wanxiao/practice/PracticeApplication.java
+++ src/main/java/com/wanxiao/practice/PracticeApplication.java
@@ -1,9 +1,20 @@
 package com.wanxiao.practice;
 
+import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 2021-01-12 14:00:47
+ *
+ * @author wanxiao
+ */
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableApiBootSwagger
+@EnableScheduling
 public class PracticeApplication {
 
     public static void main(String[] args) {
